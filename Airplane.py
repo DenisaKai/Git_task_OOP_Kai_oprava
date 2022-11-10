93fd407498b47273b9ab41ab066f52de83e43ecc
Demoma <denca.kaisrlik@gmail.com>
2022-11-10 16:21:18 +0100

    commit price

--- Airplane.py
+++ Airplane.py
@@ -1,7 +1,8 @@
 #This is a Airplane
 class Airplane:
 
-    def __init__(self, name, color, typ):
+    def __init__(self, name, color, typ, price):
+        self.price = price
         self.typ = typ
         self.name = name
         self.color = color
--- Motorbike.py
+++ Motorbike.py
@@ -2,7 +2,8 @@
 
 class Motorbike:
 
-    def __init__(self, name, color, typ):
+    def __init__(self, name, color, typ, price):
+        self.price = price
         self.typ = typ
         self.name = name
         self.color = color
